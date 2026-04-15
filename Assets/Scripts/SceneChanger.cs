54daf3ff4550ce224ee09166839214cecc7dae62
freedomryu <dn910402@gmail.com>
2026-04-15 19:23:21 +0800

    unity_class2

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created 我
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
