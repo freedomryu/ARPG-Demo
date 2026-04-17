3f9245f1945a7a689907684667df07b54fd22df3
freedomryu <dn910402@gmail.com>
2026-04-17 12:41:40 +0800

    Unity1_Lcc_ExitPanel

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created 我
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
